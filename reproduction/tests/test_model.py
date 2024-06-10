a9e6a6c255ab6bdd27bd5a470a09c747bcea561a
amyheather <a.heather2@exeter.ac.uk>
2024-06-10 09:53:20 +0100

    Test that successfully compares dataframes (but currently just comparing the expected result with itself, not running the model)

--- reproduction/tests/test_model.py
+++ reproduction/tests/test_model.py
@@ -7,72 +7,49 @@ producing consistent results.
 
 import pytest
 import pandas as pd
-import sim.sim_replicate as sim
-from sim.parameters import Scenario
+# import sim.sim_replicate as sim
+# from sim.parameters import Scenario
 from pathlib import Path
 
-DISPLACED = 'base_3_month_reps_30_displaced_audit.csv'
-PATH_DISPLACED_1 = Path(__file__).parent.joinpath('data', DISPLACED)
-PATH_DISPLACED_2 = Path(__file__).parent.joinpath('data', DISPLACED)
-
-@pytest.mark.parametrize(
-    'displaced_audit',
-    [
-        (PATH_DISPLACED_1),
-        (PATH_DISPLACED_2),
-    ],
-)
-
+'''
 def run_model():
-    '''
-    Run model with parameters from computational reproducibility assessment
-    '''
+    # Run model with parameters from computational reproducibility assessment
     scenarios = {}
     scenarios['base_3_month'] = Scenario(
         run_length=150,
         proportion_pos_requiring_inpatient=0.6)
     number_of_replications = 30
     base_random_set = 2700
-    output_folder='test_output'
+    output_folder = 'test_output'
     sim.run_replications(
         scenarios, number_of_replications, base_random_set, output_folder)
-
-#def test_displaced(displaced_audit):
-#    '''
-#    Test that results are consistent with those from the computational
-#    reproducibility assessment
-#    '''
-#    displaced_data = pd.read_csv(displaced_audit)
-
 '''
-# Import model
-import sim.sim_replicate as sim
-from sim.parameters import Scenario
 
-def test_reproduction():
-    # Test run model as set-up for computational reproducibility assessment.
-    # Parameters
-    scenarios = {}
-    scenarios['base_3_month'] = Scenario(
-        run_length=150,
-        proportion_pos_requiring_inpatient=0.6)
-    number_of_replications = 30
-    base_random_set = 2700
-    output_folder='test_output'
+# TO DO: Try removing the init files and see if it works still
+# TO DO: Correct test path to test folder
+# Paths to expected and test outputs from the model
+DISPLACED = 'base_3_month_reps_30_displaced_audit.csv'
+INPATIENT = 'base_3_month_reps_30_inpatient_audit.csv'
+PATIENT = 'base_3_month_reps_30_patient_audit.csv'
+UNIT = 'base_3_month_reps_30_unit_audit.csv'
+EXP_FOLDER = 'data'
+TEST_FOLDER = 'data'
 
-    # Import expected results
-    displaced = pd.read_csv('data/base_3_month_reps_30_displaced_audit.csv')
-    inpatient = pd.read_csv('data/base_3_month_reps_30_inpatient_audit.csv')
-    patient = pd.read_csv('data/base_3_month_reps_30_patient_audit.csv')
-    unit = pd.read_csv('data/base_3_month_reps_30_unit_audit.csv')
+# Create empty list to populate with tuples of paths to test and expected files
+paths = []
+for file in [DISPLACED, INPATIENT, PATIENT, UNIT]:
+    test_path = Path(__file__).parent.joinpath(TEST_FOLDER, file)
+    exp_path = Path(__file__).parent.joinpath(EXP_FOLDER, file)
+    paths.append((test_path, exp_path))
 
-    # Run the simulation
-    sim.run_replications(
-        scenarios, number_of_replications, base_random_set, output_folder)
 
-    # Import the test results
-    test_displaced = pd.read_csv('test_output/base_3_month_reps_30_displaced_audit.csv')
-
-    # Compare against actual results
-    assert pd.testing.assert_frame_equal(displaced, test_displaced)
-'''
+# Repeat function on each of the tuples in `paths`
+@pytest.mark.parametrize('test_df, expected_df', paths)
+def test_equal_df(test_df, expected_df):
+    '''
+    Test that results are consistent with the expected results (which are
+    from the computational reproducibility assessment)
+    '''
+    test_result = pd.read_csv(test_df)
+    expected_result = pd.read_csv(expected_df)
+    pd.testing.assert_frame_equal(test_result, expected_result)
